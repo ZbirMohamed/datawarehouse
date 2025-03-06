074bc931c8bf4ec41706044ddda9deb313297675
Zbir <aminezbir09@gmail.com>
2025-03-06 21:04:49 +0000

    Add the files

---
+++ scripts/database/__init__.py
@@ -0,0 +1,1 @@
+from .db import engine_maker
---
+++ scripts/sql.sql
@@ -0,0 +1,10 @@
+CREATE IF NOT EXISTS customers(
+    InvoiceNo	varchar(255),
+    StockCode	varchar(255),
+    Description	varchar(255),
+    Quantity	int,
+    InvoiceDate	timestamp,
+    UnitPrice	float,
+    CustomerID	float,
+    Country     varchar(255)
+);
